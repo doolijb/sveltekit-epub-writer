f279586cb485bf972986b8c42c976075bd4638d1
Jody Doolittle <jody@doolittle.me>
2025-03-20 02:52:32 -0700

    make db url optional

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,8 +2,8 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = createClient({ url: env.DATABASE_URL });
+// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = createClient({ url: env.DATABASE_URL || 'file:local.db' });
 
 export const db = drizzle(client, {
   schema
